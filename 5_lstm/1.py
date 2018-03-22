524124498c72db0ccc559a9b6f12adcb6670dea4
ravitejajnv <32868337+ravitejajnv@users.noreply.github.com>
2018-03-22 16:21:18 +0530

    Update 1.py

--- 5_lstm/1.py
+++ 5_lstm/1.py
@@ -1,1 +1,33 @@
-##
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+
+
+# In[5]:
+
+
+train_dataset = dsets.MNIST(root='../ffnn/data',
+                           train=True,
+                           transform=transforms.ToTensor(),
+                           download=True)
+test_dataset = dsets.MNIST(root='../ffnn/data',
+                          train=False,
+                          transform=transforms.ToTensor())
+
+
+# In[6]:
+
+
+batch_size = 100
+n_iters = 3000
+num_epochs = n_iters / (len(train_dataset)/batch_size)
+num_epochs = int(num_epochs)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                         batch_size=batch_size,
+                                         shuffle=False)
